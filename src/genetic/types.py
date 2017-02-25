9c7571f547feba30d27eec543b2ce12795d467ac
Ehud <ehudhala@hotmail.com>
2017-02-25 20:02:35 +0200

    Some more utils for a general GA.

--- src/genetic/types.py
+++ src/genetic/types.py
@@ -1,7 +1,8 @@
-from typing import TypeVar, List
+from typing import TypeVar, List, Callable, Tuple
 
 Locus = TypeVar('Locus', [int])
 Chromosome = List[Locus]
 Population = List[Chromosome]
 
 Fitness = float
+CrossoverFunction = Callable[[Chromosome, Chromosome], Tuple[Chromosome, Chromosome]]
--- src/genetic/utils.py
+++ src/genetic/utils.py
@@ -1,13 +1,15 @@
-import random
-
-import itertools
 import bisect
-from typing import List, Tuple
+import itertools
+import random
+from typing import List, Tuple, Callable
 
-from genetic.types import Chromosome, Fitness, Population
+from genetic.types import Chromosome, Fitness, Population, CrossoverFunction
 
 
-def proportionate_selection(population: Population, fitnesses: List[Fitness], rand: random.Random=None) -> Chromosome:
+# Selection
+
+def proportionate_selection(population: Population, fitnesses: List[Fitness],
+                            rand=None) -> Chromosome:
     """
     In roulette wheel selection we receive a population of individuals,
     and select one of them (to be a parent) with proportion to their fitness.
@@ -22,11 +24,13 @@ def proportionate_selection(population: Population, fitnesses: List[Fitness], ra
     accu_fitnesses = list(itertools.accumulate(fitnesses))
     chosen = rand.uniform(0, accu_fitnesses[-1])
 
-    chosen_individual_index = min(bisect.bisect_right(accu_fitnesses, chosen), len(population) - 1)
+    chosen_individual_index = bisect.bisect_right(accu_fitnesses, chosen)
+    chosen_individual_index = min(chosen_individual_index, len(population) - 1)
     return population[chosen_individual_index]
 
 
-def select_chromosomes(population: Population, fitnesses: List[Fitness], amount: int) -> Tuple[Chromosome, ...]:
+def select_chromosomes(population: Population, fitnesses: List[Fitness],
+                       amount: int) -> Tuple[Chromosome, ...]:
     """
     Gets a population, and selects from the population the amount of chromosomes wanted.
     The selection is a proportionate selection.
@@ -39,4 +43,56 @@ def select_chromosomes(population: Population, fitnesses: List[Fitness], amount:
     """
     return tuple(proportionate_selection(population, fitnesses)
                  for _ in range(amount))
+
+
+# Crossover
+
+def crossover(chrom_a: Chromosome, chrom_b: Chromosome, actual_crossover: CrossoverFunction,
+              crossover_rate: float, rand: random.Random=None) -> Tuple[Chromosome, Chromosome]:
+    """
+    Gets two chromosomes, and crosses them over.
+    There is a (high) chance that they will "mate" and create two offspring,
+    and a chance that they will survive and continue to the next generation.
+    :param chrom_a: The first chromosome.
+    :param chrom_b: The second chromosome.
+    :param actual_crossover: The crossover function that will happen if they "mate".
+    :return: Two offspring chromosomes.
+    :param crossover_rate: The rate at which chromosomes actually cross over.
+    """
+    rand = rand or random
+
+    if rand.random() < crossover_rate:
+        return actual_crossover(chrom_a, chrom_b)
+    else:
+        return chrom_a, chrom_b
+
+
+def generation(population: Population, fitness: Callable[[Chromosome], Fitness],
+               actual_crossover: CrossoverFunction, crossover_rate: float,
+               mutator: Callable[[Chromosome], Chromosome]) -> Population:
+    """
+    Performs a single generation of the algorithm.
+    In a generation we create a new population with the same size.
+    We proportionately select (with replacement) two chromosomes,
+    cross them over and mutate the offspring until our population is big enough.
+    The reason this function has so many parameters is because this is the function
+    to run on any genetic algorithm.
+    :param population: The population to pass through one generation.
+    :param fitness: The fitness function to get the fitness of a chromosome.
+    :param actual_crossover: The function that gets two chromosomes, and actually crosses them over.
+    :param crossover_rate: The rate at which Crossover will happen.
+    :param mutator: The function that receives a chromosomes and mutates it (randomly)
+    :return: The population after one generation.
+    """
+    fitnesses = list(map(fitness, population))
+
+    new_population = []
+    while len(new_population) < len(population):
+        chrom_a, chrom_b = select_chromosomes(population, fitnesses, 2)
+
+        offspring = crossover(chrom_a, chrom_b, actual_crossover, crossover_rate)
+
+        new_population.extend(map(mutator, offspring))
+
+    return new_population
 
